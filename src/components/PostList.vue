7a75696e83bd1544aa84c2417c60d0c108b58bb7
Angel <angelkysinn@gmail.com>
2021-04-26 16:46:13 -0700

    Add styling

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -13,14 +13,5 @@ import SinglePost from './SinglePost.vue'
 export default {
   props: ['posts'],
   components: { SinglePost },
-  setup(props) {
-    // onMounted(() => console.log('component mounted'))
-    // onUnmounted(() => console.log('component unmounted'))
-    // onUpdated(() => console.log('component updated'))
-  }
 }
 </script>
-
-<style>
-
-</style>
--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -7,10 +7,10 @@ const getPosts = () => {
   const load = async () => {
     try {
       // simulate delay
-      await new Promise(resolve => {
-        setTimeout(resolve, 2000)
-      })
-      
+      // await new Promise(resolve => {
+      //   setTimeout(resolve, 2000)
+      // })
+
       let data = await fetch('http://localhost:3000/posts')
       // console.log(data);
       if (!data.ok) {
