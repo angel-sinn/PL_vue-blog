8c80c2a33adb82b73c25148dae6017400507bebc
Angel <angelkysinn@gmail.com>
2021-04-26 13:40:36 -0700

    Watch vs watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <input type="text" v-model="search">
+    <p>search term = {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click='handleClick'>stop watching</button>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
   // composition api - variables not reactive by default, but can use refs to make it reactive
   setup() {
-    const ninjaOne = ref({ name: 'mario', age: 30 })
-    const ninjaTwo = reactive({ name: 'luigi', age: 35 })
+    const search = ref("")
+    const names = ref(['mario', 'luigi', 'yoshi', 'toad', 'bowser', 'koopa', 'peach'])
 
-    // refs
-    const updateNinjaOne = () => {
-      // need to use .value to change it (only inside setup)
-      ninjaOne.value.age = 40
+    const stopWatch = watch(search, () => {
+      console.log('watch function run');
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function run', search.value);
+    })
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
     }
 
-    // reactive - doesn't work with primitive values
-    const updateNinjaTwo = () => {
-      // can access properties directly on it, no need to use .value
-      ninjaTwo.age = 45
-    }
-
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
-  },
+    return { names, search, matchingNames, handleClick }
+  }
 }
 </script>
