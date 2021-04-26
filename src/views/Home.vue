d5f98c0713f78a2f685f0b93f090c1edd1b25640
Angel <angelkysinn@gmail.com>
2021-04-26 13:25:12 -0700

    Refs vs reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="home">
-    Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'Home',
   // composition api - variables not reactive by default, but can use refs to make it reactive
   setup() {
+    const ninjaOne = ref({ name: 'mario', age: 30 })
+    const ninjaTwo = reactive({ name: 'luigi', age: 35 })
 
-    // const p = ref(null)
-    
-    const name = ref('mario')
-    const age = ref(30)
-
-    const handleClick = () => {
+    // refs
+    const updateNinjaOne = () => {
       // need to use .value to change it (only inside setup)
-      name.value = 'luigi'
-      age.value = 35
+      ninjaOne.value.age = 40
     }
 
-    return {name, age, handleClick}
+    // reactive - doesn't work with primitive values
+    const updateNinjaTwo = () => {
+      // can access properties directly on it, no need to use .value
+      ninjaTwo.age = 45
+    }
+
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
   },
 }
 </script>
