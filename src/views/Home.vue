99f1c327687547252f321ddd2f85180ae8a0fa0f
Angel <angelkysinn@gmail.com>
2021-04-26 13:53:09 -0700

    Using props in setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,25 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term = {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click='handleClick'>stop watching</button>
+    <h1>My Lighthouse Labs Journey</h1>
+    <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
   name: 'Home',
+  components: { PostList },
   // composition api - variables not reactive by default, but can use refs to make it reactive
   setup() {
-    const search = ref("")
-    const names = ref(['mario', 'luigi', 'yoshi', 'toad', 'bowser', 'koopa', 'peach'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function run');
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function run', search.value);
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-    return { names, search, matchingNames, handleClick }
+    const posts = ref([
+      { title: 'Pre Lighthouse Labs', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', id: 1 },
+      { title: 'Week 1', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', id: 2 },
+    ])
+    
+    return { posts }
   }
 }
 </script>
