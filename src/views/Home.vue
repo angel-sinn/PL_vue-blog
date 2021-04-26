1e3fd3f33b614d1c44447adfd1cb4de835272205
Angel <angelkysinn@gmail.com>
2021-04-26 13:13:40 -0700

    Using refs to change values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="home">
     Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
+  // composition api - variables not reactive by default, but can use refs to make it reactive
   setup() {
 
-  },
-  created() {
-
-  },
-  mounted() {
+    // const p = ref(null)
     
-  }
+    const name = ref('mario')
+    const age = ref(30)
+
+    const handleClick = () => {
+      // need to use .value to change it (only inside setup)
+      name.value = 'luigi'
+      age.value = 35
+    }
+
+    return {name, age, handleClick}
+  },
 }
 </script>
